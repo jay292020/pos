de5ed4c71550f72ea7605fcc1ca14c55ed269f4d
Shaik <sandhani.sk45@gmail.com>
2020-09-29 20:00:16 +0530

    fix: added apiKey

--- src/service/pos.js
+++ src/service/pos.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 const endPoint = "http://54.160.141.173:3010/api/vendor/pointOfSaleErrors";
-export const getPointOfSaleErrors = (key,id, apiKey) => {
-    return axios.get(`${endPoint}`).then((res) => res.data);
+let apiKey =  window.location.href.split('=')[1] ? window.location.href.split('=')[1] : 0
+export const getPointOfSaleErrors = () => {
+    return axios.get(`${endPoint}`,{
+        headers: {
+            api_key: apiKey
+        }
+    }).then((res) => res.data);
 };
 export const putPointOfSaleErrors = (key,body) => {
-    return axios.put(`${endPoint}/${key}`,body).then((res) => res.data);
+    return axios.put(`${endPoint}/${key}`,body,{
+        headers: {
+            api_key: apiKey
+        }
+    }).then((res) => res.data);
 };
